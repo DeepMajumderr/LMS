7713f45e23ac89be7bf8fbed6eeff9eeb0df1f8f
Deep <majumderdeep06@gmail.com>
2025-06-22 12:49:29 +0530

    stripe added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,13 +2,17 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from 'humanize-duration'
+import { useAuth, useUser } from "@clerk/clerk-react"
 
-export const  AppContext = createContext()
+export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY
     const navigate = useNavigate()
+
+    const { getToken } = useAuth()
+    const { user } = useUser()
 
     const [allCourses, setallCourses] = useState([])
     const [isEducator, setisEducator] = useState(true)
@@ -21,10 +25,10 @@ export const AppContextProvider = (props) => {
 
     //Function to calculate the avg rating of a course
     const calculateRating = (course) => {
-        if(course.courseRatings.length === 0) {
+        if (course.courseRatings.length === 0) {
             return 0;
         }
-        let  totalRating = 0
+        let totalRating = 0
         course.courseRatings.forEach(rating => {
             totalRating += rating.rating
         })
@@ -34,25 +38,25 @@ export const AppContextProvider = (props) => {
     //Function to calculate coursechapter time
     const calculateChapterTime = (chapter) => {
         let time = 0;
-        chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration)
-        return humanizeDuration(time * 60 * 1000, {units: ['h', 'm']})
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] })
     }
 
     //Function to calculate course duration
     const calculateCourseDuration = (course) => {
         let time = 0;
 
-        course.courseContent.map((chapter)=> chapter.chapterContent.map(
+        course.courseContent.map((chapter) => chapter.chapterContent.map(
             (lecture) => time += lecture.lectureDuration
         ))
-        return humanizeDuration(time * 60 * 1000, {units: ['h', 'm']})
+        return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] })
     }
 
     //Function to calculate number of lectures in the course
-    const calculateNoOfLectures = (course)=>{
+    const calculateNoOfLectures = (course) => {
         let totalLectures = 0;
         course.courseContent.forEach(chapter => {
-            if(Array.isArray(chapter.chapterContent)){
+            if (Array.isArray(chapter.chapterContent)) {
                 totalLectures += chapter.chapterContent.length;
             }
         })
@@ -60,7 +64,7 @@ export const AppContextProvider = (props) => {
     }
 
     // Fetch user enrolled courses
-    const fetchUserEnrolledCourses = async () =>{
+    const fetchUserEnrolledCourses = async () => {
         setenrolledCourses(dummyCourses)
     }
 
@@ -68,15 +72,27 @@ export const AppContextProvider = (props) => {
         fetchAllCourses()
         fetchUserEnrolledCourses()
     }, [])
-    
+
+    const logToken = async () => {
+        console.log(await getToken());
+
+    }
+
+    useEffect(() => {
+        if (user) {
+            logToken()
+        }
+    }, [user])
+
+
 
     const value = {
         currency,
         allCourses,
         navigate, calculateRating,
         isEducator, setisEducator,
-        calculateChapterTime,calculateCourseDuration,calculateNoOfLectures,
-        enrolledCourses,fetchUserEnrolledCourses
+        calculateChapterTime, calculateCourseDuration, calculateNoOfLectures,
+        enrolledCourses, fetchUserEnrolledCourses
     }
 
     return (
